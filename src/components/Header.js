4fbe9e99d61b3a01f67eaeb7f5cf37c69240b0e5
tylermcmu <94275794+tylermcmu@users.noreply.github.com>
2022-09-04 16:25:13 -0700

    updates to nav structure and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import Nav from "./Nav";
 
 const Header = () => {
     return (
-        <header>
-        <h1>Stranger's Things</h1>
+        <header className="d-flex p-2 flex-column">
+        <h1 className="text-center">Stranger's Things</h1>
         <Nav />
         </header>
     )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,21 @@ import { BrowserRouter, Link, Route, Routes, useNavigate, useParams } from 'reac
 
 const Nav = () => {
     return (
-    <div className='nav'>
-        <Link to="/"> Home</Link>
-        <Link to="/posts"> Posts</Link>
-        <Link to="/account/login"> Login</Link>
-        <Link to="/account/signup"> Sign Up</Link>
-    </div>
-
+    <nav className='nav d-flex p-2 flex-row justify-content-between'>
+        <ul className="nav nav-pills d-flex p-2 flex-row">
+            <li className="nav-item"><Link className ="border border-dark nav-link" to="/"> Home</Link></li>
+            <li className="nav-item"><Link className ="border border-dark nav-link" to="/posts"> Posts</Link></li>
+        </ul>
+        <ul className="nav nav-pills d-flex p-2 flex-row">
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Account</a>
+                <div className="dropdown-menu">
+                    <Link className ="dropdown-item" to="/account/login"> Login</Link>
+                    <Link className ="dropdown-item" to="/account/signup"> Sign Up</Link>
+                </div>
+            </li>
+        </ul>
+    </nav>
     )
 }
 
